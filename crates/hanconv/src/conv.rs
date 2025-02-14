a4a9f517c3149df9eb348d363cd70d42bb68c24a
fhluo <20843637+fhluo@users.noreply.github.com>
2025-02-15 04:37:12 +0800

    feat: add conversion functions

--- crates/hanconv/src/conv.rs
+++ crates/hanconv/src/conv.rs
@@ -1,8 +1,5 @@
-use crate::{
-    HKVariants, HKVariantsRev, HKVariantsRevPhrases, JPShinjitaiCharacters, JPShinjitaiPhrases,
-    JPVariants, JPVariantsRev, STCharacters, STPhrases, TSCharacters, TSPhrases, TWPhrases,
-    TWPhrasesRev, TWVariants, TWVariantsRev, TWVariantsRevPhrases,
-};
+use crate::Dictionary::*;
+use std::sync::LazyLock;
 use trie::Trie;
 
 pub enum Convertors {
@@ -30,7 +27,11 @@ macro_rules! trie {
 
 impl Convertors {
     pub fn new(&self) -> Convertor {
-        Convertor(match self {
+        Convertor::new(self.dictionaries())
+    }
+
+    pub fn dictionaries(&self) -> Vec<Trie<&'static str>> {
+        match self {
             Convertors::S2T => vec![trie![STPhrases, STCharacters]],
             Convertors::S2TW => vec![trie![STPhrases, STCharacters], trie![TWVariants]],
             Convertors::S2TWP => vec![
@@ -62,24 +63,56 @@ impl Convertors {
                 JPShinjitaiCharacters,
                 JPVariantsRev
             ]],
-        })
+        }
     }
 }
 
 pub struct Convertor(Vec<Trie<&'static str>>);
 
 impl Convertor {
+    pub fn new(dictionaries: Vec<Trie<&'static str>>) -> Self {
+        Self(dictionaries)
+    }
+
     pub fn convert(&self, s: &str) -> String {
-        if let Some(trie) = self.0.first() {
-            let mut next = trie.convert(s);
+        match self.0.len() {
+            0 => s.to_string(),
+            1 => self.0[0].convert(s),
+            _ => {
+                let mut s = self.0[0].convert(s);
 
-            for trie in self.0.iter().skip(1) {
-                next = trie.convert(&next);
+                for trie in self.0.iter().skip(1) {
+                    s = trie.convert(&s);
+                }
+
+                s
             }
-
-            next
-        } else {
-            s.to_string()
         }
     }
+}
+
+macro_rules! f {
+    {$($func_name:ident($convertor:ident))+} => {
+        $(pub fn $func_name(s: &str) -> String {
+            static CONVERTOR: LazyLock<Convertor> = LazyLock::new(|| Convertors::$convertor.new());
+            CONVERTOR.convert(s)
+        })+
+    };
+}
+
+f! {
+    s2t(S2T)
+    s2tw(S2TW)
+    s2twp(S2TWP)
+    t2s(T2S)
+    t2tw(T2TW)
+    tw2s(TW2S)
+    tw2sp(TW2SP)
+    tw2t(TW2T)
+    s2hk(S2HK)
+    hk2s(HK2S)
+    hk2t(HK2T)
+    t2hk(T2HK)
+    t2jp(T2JP)
+    jp2t(JP2T)
 }
--- crates/hanconv/src/lib.rs
+++ crates/hanconv/src/lib.rs
@@ -1,5 +1,11 @@
 pub mod conv;
 pub mod dict;
 
-pub use conv::{Convertor, Convertors::*};
-pub use dict::{Dictionary, Dictionary::*};
+pub use conv::{
+    hk2s, hk2t, jp2t, s2hk, s2t, s2tw, s2twp, t2hk, t2jp, t2s, t2tw, tw2s, tw2sp, tw2t,
+};
+pub use conv::{
+    Convertor,
+    Convertors::{self, *},
+};
+pub use dict::Dictionary::{self, *};
