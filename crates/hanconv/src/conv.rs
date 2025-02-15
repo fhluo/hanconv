2886691f74c7679f3aba3b009f06bdc5b16f595b
fhluo <20843637+fhluo@users.noreply.github.com>
2025-02-15 18:49:59 +0800

    docs: add documents for Convertors

--- crates/hanconv/src/conv.rs
+++ crates/hanconv/src/conv.rs
@@ -3,19 +3,61 @@ use crate::Dictionary::*;
 use std::sync::LazyLock;
 
 pub enum Convertors {
+    /// Simplified Chinese to Traditional Chinese
+    ///
+    /// 简体中文 → 繁体中文
     S2T,
-    S2TW,
-    S2TWP,
+    /// Traditional Chinese to Simplified Chinese
+    ///
+    /// 繁体中文 → 简体中文
     T2S,
-    T2TW,
+    /// Simplified Chinese to Traditional Chinese (Taiwan)
+    ///
+    /// 简体中文 → 繁体中文（台湾）
+    S2TW,
+    /// Traditional Chinese (Taiwan) to Simplified Chinese
+    ///
+    /// 繁体中文（台湾）→ 简体中文
     TW2S,
+    /// Simplified Chinese to Traditional Chinese (Taiwan) with Taiwanese idiom
+    ///
+    /// 简体中文 → 繁体中文（台湾），转换为台湾常用词
+    S2TWP,
+    /// Traditional Chinese (Taiwan) to Simplified Chinese with Mainland Chinese idiom
+    ///
+    /// 繁体中文（台湾）→ 简体中文，转化为中国大陆常用词
     TW2SP,
+    /// Traditional Chinese to Traditional Chinese (Taiwan)
+    ///
+    /// 繁体中文 → 繁体中文（台湾）
+    T2TW,
+    /// Traditional Chinese (Taiwan) to Traditional Chinese
+    ///
+    /// 繁体中文（台湾）→ 繁体中文
     TW2T,
+    /// Simplified Chinese to Traditional Chinese (Hong Kong)
+    ///
+    /// 简体中文 → 繁体中文（香港）
     S2HK,
+    /// Traditional Chinese (Hong Kong) to Simplified Chinese
+    ///
+    /// 繁体中文（香港）→ 简体中文
     HK2S,
-    HK2T,
+    /// Traditional Chinese to Traditional Chinese (Hong Kong)
+    ///
+    /// 繁体中文 → 繁体中文（香港）
     T2HK,
+    /// Traditional Chinese (Hong Kong) to Traditional Chinese
+    ///
+    /// 繁体中文（香港）→ 繁体中文
+    HK2T,
+    /// Traditional Chinese characters (Kyūjitai) to New Japanese Kanji (Shinjitai)
+    ///
+    /// 繁体字 → 日文新字体
     T2JP,
+    /// New Japanese Kanji (Shinjitai) to Traditional Chinese characters (Kyūjitai)
+    ///
+    /// 日文新字体 → 繁体字
     JP2T,
 }
 
