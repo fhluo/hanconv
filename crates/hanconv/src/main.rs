aceeb9cb1a0ffec54ca580c85421f225694e080a
fhluo <20843637+fhluo@users.noreply.github.com>
2025-02-16 11:43:30 +0800

    refactor: rename Args_ to ConvertArgs for clarity and consistency

--- crates/hanconv/src/main.rs
+++ crates/hanconv/src/main.rs
@@ -16,62 +16,62 @@ enum Commands {
     /// Simplified Chinese to Traditional Chinese
     ///
     /// 简体中文 → 繁体中文
-    S2T(Args_),
+    S2T(ConvertArgs),
     /// Traditional Chinese to Simplified Chinese
     ///
     /// 繁体中文 → 简体中文
-    T2S(Args_),
+    T2S(ConvertArgs),
     /// Simplified Chinese to Traditional Chinese (Taiwan)
     ///
     /// 简体中文 → 繁体中文（台湾）
-    S2TW(Args_),
+    S2TW(ConvertArgs),
     /// Traditional Chinese (Taiwan) to Simplified Chinese
     ///
     /// 繁体中文（台湾）→ 简体中文
-    TW2S(Args_),
+    TW2S(ConvertArgs),
     /// Simplified Chinese to Traditional Chinese (Taiwan) with Taiwanese idiom
     ///
     /// 简体中文 → 繁体中文（台湾），转换为台湾常用词
-    S2TWP(Args_),
+    S2TWP(ConvertArgs),
     /// Traditional Chinese (Taiwan) to Simplified Chinese with Mainland Chinese idiom
     ///
     /// 繁体中文（台湾）→ 简体中文，转化为中国大陆常用词
-    TW2SP(Args_),
+    TW2SP(ConvertArgs),
     /// Traditional Chinese to Traditional Chinese (Taiwan)
     ///
     /// 繁体中文 → 繁体中文（台湾）
-    T2TW(Args_),
+    T2TW(ConvertArgs),
     /// Traditional Chinese (Taiwan) to Traditional Chinese
     ///
     /// 繁体中文（台湾）→ 繁体中文
-    TW2T(Args_),
+    TW2T(ConvertArgs),
     /// Simplified Chinese to Traditional Chinese (Hong Kong)
     ///
     /// 简体中文 → 繁体中文（香港）
-    S2HK(Args_),
+    S2HK(ConvertArgs),
     /// Traditional Chinese (Hong Kong) to Simplified Chinese
     ///
     /// 繁体中文（香港）→ 简体中文
-    HK2S(Args_),
+    HK2S(ConvertArgs),
     /// Traditional Chinese to Traditional Chinese (Hong Kong)
     ///
     /// 繁体中文 → 繁体中文（香港）
-    T2HK(Args_),
+    T2HK(ConvertArgs),
     /// Traditional Chinese (Hong Kong) to Traditional Chinese
     ///
     /// 繁体中文（香港）→ 繁体中文
-    HK2T(Args_),
+    HK2T(ConvertArgs),
     /// Traditional Chinese characters (Kyūjitai) to New Japanese Kanji (Shinjitai)
     ///
     /// 繁体字 → 日文新字体
-    T2JP(Args_),
+    T2JP(ConvertArgs),
     /// New Japanese Kanji (Shinjitai) to Traditional Chinese characters (Kyūjitai)
     ///
     /// 日文新字体 → 繁体字
-    JP2T(Args_),
+    JP2T(ConvertArgs),
 }
 
-fn convert(convertor: Convertor, args: Args_) -> io::Result<()> {
+fn convert(convertor: Convertor, args: ConvertArgs) -> io::Result<()> {
     if let Some(items) = args.items {
         for item in items {
             println!("{}", convertor.convert(&item));
@@ -120,7 +120,7 @@ impl Commands {
 }
 
 #[derive(Args)]
-struct Args_ {
+struct ConvertArgs {
     /// Input file
     #[arg(short, value_name = "PATH")]
     input: Option<String>,
