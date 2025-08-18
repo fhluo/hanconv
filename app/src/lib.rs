1b0ef3aa91b9e39f32167b09e105d95572f96936
fhluo <20843637+fhluo@users.noreply.github.com>
2025-08-18 08:25:32 +0800

    chore: update permissions and add plugins

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -75,6 +75,9 @@ async fn jp2t(s: String) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
 
