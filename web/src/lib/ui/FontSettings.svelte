82f94c6f8385b073a329d994765ab3f4c9c2bdbf
fhluo <20843637+fhluo@users.noreply.github.com>
2025-12-19 19:13:31 +0800

    feat: add FontSettings

---
+++ web/src/lib/ui/FontSettings.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { Type } from "@lucide/svelte";
+  import FontFamilySelector from "./FontFamilySelector.svelte";
+  import FontSizeControl from "./FontSizeControl.svelte";
+
+  let isOpen = $state(false);
+</script>
+
+<div class="relative">
+  <button
+    onclick={() => {
+      isOpen = !isOpen;
+    }}
+    class={[
+      "p-1.5 rounded-md text-gray-500 dark:text-gray-400 outline-none",
+      "transition-colors hover:bg-gray-100 dark:hover:bg-gray-800",
+      isOpen ? "bg-gray-100 dark:bg-gray-800" : "",
+    ]}
+  >
+    <Type size={14} />
+  </button>
+
+  {#if isOpen}
+    <div
+      class="fixed inset-0 z-40"
+      onclick={() => (isOpen = false)}
+      role="presentation"
+    ></div>
+
+    <div
+      class={[
+        "absolute flex flex-col gap-2 bottom-full right-0 mb-2 min-w-50 p-2 z-50",
+        "shadow-lg rounded-lg bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800",
+        "animate-in fade-in zoom-in-95 slide-in-from-bottom-2 duration-200 ease-out origin-bottom-right",
+      ]}
+    >
+      <FontFamilySelector />
+      <div class="h-px my-1 bg-gray-100 dark:bg-gray-800"></div>
+      <FontSizeControl />
+    </div>
+  {/if}
+</div>
