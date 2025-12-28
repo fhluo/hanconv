3d4c36f5c72db28cbcaf12a8184c322db1466c89
fhluo <20843637+fhluo@users.noreply.github.com>
2025-12-28 23:57:40 +0800

    chore: add clipboard-manager

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -83,6 +83,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
