962e9974eaedb048561278c1c2649f9cf138aa64
fhluo <20843637+fhluo@users.noreply.github.com>
2026-01-14 00:40:59 +0800

    chore: add app-icon.ico

--- crates/hanconv-app/build.rs
+++ crates/hanconv-app/build.rs
@@ -1,10 +1,11 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=locales");
+    println!("cargo:rerun-if-changed=assets");
 
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         winresource::WindowsResource::new()
-            .set_icon("icon.ico")
+            .set_icon("assets/icons/app-icon.ico")
             .compile()
             .unwrap();
     }
